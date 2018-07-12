8d84ac866aa5ffe368679eb01e424052917b92f9
Rafael <raphab33@hotmail.com>
2018-07-11 23:12:09 -0300

    Created class Lembrete

---
+++ src/main/java/br/com/rapha/java/hibernate/Lembrete.java
@@ -0,0 +1,59 @@
+package br.com.rapha.java.hibernate;
+
+public class Lembrete {
+
+
+    private long id;
+
+
+    private String titulo;
+
+
+    private String descricao;
+
+    public Lembrete() {
+    }
+
+    public Lembrete(long id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Lembrete{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
+
+
