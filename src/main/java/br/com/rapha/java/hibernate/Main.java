5da865a297f5287488fc1d655a1625ba3cd11a79
Rafael <raphab33@hotmail.com>
2018-07-12 00:16:52 -0300

    Seeking per id and REMOVE 'lembretes' with EntityManager.remove

--- src/main/java/br/com/rapha/java/hibernate/Main.java
+++ src/main/java/br/com/rapha/java/hibernate/Main.java
@@ -17,17 +17,13 @@ public class Main {
         try {
             Lembrete lembrete = em.find(Lembrete.class, 1L);
 
-
-            lembrete.setDescricao("5:00h");
-            lembrete.setTitulo("Estudar PHP");
-
             em.getTransaction().begin();
-            em.merge(lembrete);
+            em.remove(lembrete);
             em.getTransaction().commit();
         }
         catch (Exception e){
             em.getTransaction().rollback();
-            System.out.println("UPDATE: " + e.getMessage());
+            System.out.println("DELETE: " + e.getMessage());
         }
         finally {
             em.close();
